88869f8ba5a04e9175ff0d7562d7baff3c51fc6d
Khaled Ahmed <www.khaledah826@gmail.com>
2025-02-14 21:19:46 +0200

    fix footer

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -5,10 +5,11 @@ import React from "react";
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div><Header /></div>
+      <div>
+        <Header />
+      </div>
       <main className="pt-20">{children} </main>
       <Footer />
-      <h1 className="bg-white">Magdy</h1>
     </>
   );
 }
--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
-import facebook from "../../assets/faceBook.svg";
-import instagram from "../../assets/instagram.svg";
-import appStore from "../../assets/logo appstore.png";
-import googlePlay from "../../assets/logo googlestore.png";
-import logoImg from "../../assets/Insta Order.svg";
+import appStore from "@/assets/image/appStore.svg";
+import googlePlay from "@/assets/image/googlePlay.svg";
+import logo from "@/assets/image/FOoDc.png";
 import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
-
+import { Icon } from "@iconify/react";
 const Footer: React.FC = () => {
-  const t = useTranslations("common.footer");
+  const t = useTranslations("restaurant.footer");
   const { locale } = useParams();
 
   return (
     <footer
-      className={`flex flex-col items-center py-5 ${locale === "ar" ? "rtl" : ""
-        }`}
+      className={`flex flex-col items-center bg-black py-5 ${
+        locale === "ar" ? "rtl" : "ltr"
+      }`}
     >
       <div className="flex flex-col lg:flex-row justify-between items-center w-full max-w-6xl px-4">
         {/* Logo Section */}
         <div className="mb-6 lg:mb-0">
-          {/* <Image src={} alt="Logo" className="w-32" /> */}
+          <Image src={logo} alt="Logo" className="w-32" />
         </div>
 
         <div className="flex flex-col items-center lg:items-start gap-5">
@@ -33,24 +31,31 @@ const Footer: React.FC = () => {
               </Link>
             </p>
             <p className="mb-0">{t("Follow Us")}</p>
-            {/* <Image src={facebook} alt="Facebook" className="w-6" /> */}
-            {/* <Image src={instagram} alt="Instagram" className="w-6" /> */}
+            <Icon icon="logos:facebook" width="256" height="256" />
+            <Icon icon="skill-icons:instagram" width="256" height="256" />
           </div>
           <div className="flex gap-4">
             <button className="border-0">
-              {/* <Image src={appStore} alt="Download on the App Store" className="w-32"/> */}
+              <Image
+                src={appStore}
+                alt="Download on the App Store"
+                className="w-32"
+              />
             </button>
             <button type="button" title="btn" className="border-0">
-              {/* <Image src={googlePlay} alt="Get it on Google Play" className="w-32" /> */}
+              <Image
+                src={googlePlay}
+                alt="Get it on Google Play"
+                className="w-32"
+              />
             </button>
           </div>
         </div>
       </div>
 
-      {/* Copyright Section */}
       <div className="w-full max-w-6xl mt-5 text-center">
         <p className="text-gray-500">
-          {t("Copyright © 2023 LOGO. Design & Develop by Majec Soft")}
+          {t("Copyright © 2023 LOGO Design & Develop by Majec Soft")}
         </p>
       </div>
     </footer>
