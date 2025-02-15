81af82497793ea657fb9ddd0e4b5a88b8eb072e7
Khaled Ahmed <www.khaledah826@gmail.com>
2025-02-15 15:20:57 +0200

    install shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        main: "#FD7E7E",
-        primary: "#3A98EA",
-      },
-    },
+  	extend: {
+  		colors: {
+  			main: '#FD7E7E',
+  			primary: '#3A98EA'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
 
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
