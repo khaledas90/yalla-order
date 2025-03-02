a9f39d0b4aa791e07ce94a9ca76189bdabbcc4dd
magdy Abo El-nour <mm246344@gmail.com>
2025-03-02 15:23:24 +0200

    edit config.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,7 @@ export default {
       colors: {
         main: "#FD7E7E",
         primary: "#3A98EA",
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+      }
     },
   },
 
