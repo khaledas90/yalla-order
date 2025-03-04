4f34c76835fbe105b78087ec94868b2bc572ade4
magdy Abo El-nour <mm246344@gmail.com>
2025-03-05 00:59:21 +0200

    add restaurant card component and integrate into restaurants page

--- src/app/[locale]/(main)/restaurants/page.tsx
+++ src/app/[locale]/(main)/restaurants/page.tsx
@@ -2,24 +2,29 @@ import * as React from "react";
 import { useTranslations } from "next-intl";
 import { cabinSketch } from "@/utils/fonts";
 import SearchInput from "@/components/Inputs/SearchInput";
+import Restaurants from "./_components"
 
 const Page = () => {
     const t = useTranslations("common.restaurants");
 
     return (
         <>
-            <div
-                className={`hero h-[50vh] lg:mb-[20rem] md:mb-[48rem] sm:mb-[55rem] mb-[55rem]`}
-            >
-                <div className="flex flex-col justify-center items-center h-full ">
-                    <h2
-                        className={`text-center text-6xl text-white ${cabinSketch.className} font-bold mb-[60px] `}
-                    >
-                        {t("Restaurants")}
-                    </h2>
-                    <SearchInput query="" isIconSearch isIconLocation />
+            <div className="h-full">
+                <div
+                    className={`hero h-[70vh]`}
+                >
+                    <div className="flex flex-col justify-center items-center h-full ">
+                        <h2
+                            className={`text-center text-6xl text-white ${cabinSketch.className} font-bold mb-[60px] `}
+                        >
+                            {t("Restaurants")}
+                        </h2>
+                        <div className="w-1/2">
+                            <SearchInput query="" isIconSearch isIconLocation />
+                        </div>
+                    </div>
                 </div>
-                {/* <Profile /> */}
+                <Restaurants />
             </div>
         </>
     );
